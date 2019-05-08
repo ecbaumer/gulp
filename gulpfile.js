c51ede7500f5a690b560009f3675128aa0244c00
Eric Baum <ecbaum@ericb.local>
2019-05-08 10:19:30 -0400

    second

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,19 @@ var sass = require('gulp-sass');
 
 var gulp = require('gulp');
 
-gulp.watch('app/*.sass', gulp.task('sass'));
+gulp.watch('.app/*.scss', gulp.task('sass'));
 //gulp.watch('app/js/*.js', ['js']);
 
 
 gulp.task('sass', function(){
-  return gulp.src('app/style.scss')
+  return gulp.src('./app/style.scss')
   .pipe(sass())
   //.pipe(ccsnan())
-  .pipe(gulp.dest('dist/css'));
+  .pipe(gulp.dest('./dist/css'));
 
 });
 
 gulp.task('watch', function () {
-  gulp.watch('app/*.scss', gulp.series('sass'));
+  gulp.watch('./app/*.scss', gulp.series('sass'));
   //gulp.watch('app/js/**/*.js', ['js']);
 });
